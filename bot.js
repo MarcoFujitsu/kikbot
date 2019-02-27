6168d235ed35b2b580305d5da69d5bd2f8869630
Marco Littel <marco.littel@ts.fujitsu.com>
2019-02-27 13:05:46 +0100

    ip fix

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var request = require("superagent")
  app.set('port', process.env.PORT || 5000)
  app.use(bodyParser.json())
  
- const config = { url: 'http://87d33a8d.ngrok.io', connectorId: 'fa14e3cd-60ff-41e3-8977-28ebe010f502' }
+ const config = { url: 'http://83.82.200.228:2424', connectorId: 'fa14e3cd-60ff-41e3-8977-28ebe010f502' }
  
    /* Get the request from the connector */
  
